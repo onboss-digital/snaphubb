63602723312ffd01127a400c6d20a2eeac30d746
Anderson Isotton <anderson@isotton.com.br>
2025-01-21 00:18:27 -0300

    Button watch now

--- Modules/Frontend/Resources/views/components/card/card_episode.blade.php
+++ Modules/Frontend/Resources/views/components/card/card_episode.blade.php
@@ -6,43 +6,26 @@
 
             @php
 
-                  $qualityOptions = [];
+                $qualityOptions = [];
 
-                   $videoLinks = $data['video_links'];
+                $videoLinks = $data['video_links'];
 
-                   foreach($videoLinks as $link) {
-                      $qualityOptions[$link->quality] = $link->url;
-                   }
+                foreach ($videoLinks as $link) {
+                    $qualityOptions[$link->quality] = $link->url;
+                }
 
-                 $qualityOptionsJson = json_encode($qualityOptions);
+                $qualityOptionsJson = json_encode($qualityOptions);
 
-               @endphp
+            @endphp
 
-
-            <button class="season-watch-btn" id="seasonWatchBtn_{{  $data['id'] }}"
-                            data-entertainment-id="{{ $data['entertainment_id'] }}"
-                            data-entertainment-type="tvshow"
-                            data-video-url="{{ Crypt::encryptString($data['video_url_input']) }}"
-                            data-movie-access="{{ $data['access'] }}"
-                            data-plan-id="{{ $data['plan_id'] }}"
-                            data-user-id="{{ auth()->id() }}"
-                            data-profile-id="{{ getCurrentProfile(auth()->id(),request()) }}"
-                            data-episode-id="{{ $data['id'] }}"
-                            data-first-episode-id="{{ $index+1 }}"
-                            data-quality-options={{ $qualityOptionsJson }}>
-                <span class="d-flex align-items-center justify-content-center gap-2">
-                    <span><i class="ph-fill ph-play"></i></span>
-                    {{__('frontend.watch_now')}}
-                </span>
-            </button>
         </div>
         <div class="season-content flex-grow-1">
             <div class="d-flex align-items-center justify-content-between">
-                <h5 class="mb-0">{{  $data['name'] }}</h5>
+                <h5 class="mb-0">{{ $data['name'] }}</h5>
             </div>
             <ul class="list-inline mt-3 mb-3 mx-0 p-0 d-flex align-items-center season-meta-list flex-wrap">
                 <li class="season-meta-list-item">
-                    <span class="season-meta">E{{ $index+1 }}</span>
+                    <span class="season-meta">E{{ $index + 1 }}</span>
                 </li>
                 <li class="season-meta-list-item">
 
@@ -50,13 +33,28 @@
 
                 </li>
                 <li class="season-meta-list-item">
-                    <span class="season-meta">{{  $data['release_date'] ? formatDate($data['release_date']) : '-' }}</span>
+                    <span
+                        class="season-meta">{{ $data['release_date'] ? formatDate($data['release_date']) : '-' }}</span>
                 </li>
             </ul>
             <p class="mt-0 mb-3 font-size-14">
-               {!! $data['description'] !!}
+                {!! $data['description'] !!}
             </p>
-            <a href="{{ route('episode-details', ['id' => $data['id']]) }} " class="fw-semibold">{{__('frontend.view_more_info')}}</a>
+            <button class="season-watch-btn fw-semibold" id="seasonWatchBtn_{{ $data['id'] }}"
+                data-entertainment-id="{{ $data['entertainment_id'] }}" data-entertainment-type="tvshow"
+                data-video-url="{{ Crypt::encryptString($data['video_url_input']) }}"
+                data-movie-access="{{ $data['access'] }}" data-plan-id="{{ $data['plan_id'] }}"
+                data-user-id="{{ auth()->id() }}" data-profile-id="{{ getCurrentProfile(auth()->id(), request()) }}"
+                data-episode-id="{{ $data['id'] }}" data-first-episode-id="{{ $index + 1 }}"
+                data-quality-options={{ $qualityOptionsJson }}
+                style="background: red;position: relative;margin-top: 25px;">
+                <span class="d-flex
+                align-items-center justify-content-center gap-2">
+                    <span><i class="ph-fill ph-play"></i></span>
+                    {{ __('frontend.watch_now') }}
+                </span>
+            </button>
+
         </div>
     </div>
 </div>
