c5774199e7d807e4c8ffaf1b615cdfaeac3fd403
Anderson Isotton <anderson@isotton.com.br>
2025-01-20 23:49:21 -0300

    Fix languages

--- lang/en/email.php
+++ lang/en/email.php
@@ -2,15 +2,15 @@
 
 return [
 
-    'welcome' => 'Bem-vindo ao SNAPHUB!',
-    'happy_to_have_you' => 'Estamos felizes em ter você conosco. Para concluir seu cadastro e ativar sua conta, clique no link abaixo:',
-    'confirm_account' => 'Confirmar Conta',
-    'copy_link' => 'Caso o botão acima não funcione, copie e cole o seguinte link em seu navegador:',
-    'link_expires' => 'Este link é pessoal e não deve ser compartilhado. Ele expira em 24 horas.',
-    'ignore_email' => 'Se você não realizou este cadastro, ignore este e-mail.',
-    'contact_us' => 'Qualquer dúvida, estamos à disposição! Entre em contato pelo e-mail',
-    'best_regards' => 'Abraços, Equipe SNAPHUB.',
-    'do_not_reply' => 'Não responda a este e-mail. Ele foi gerado automaticamente.',
-    'published_on' => 'Publicado em',
+    'welcome' => 'Welcome to SNAPHUB!',
+    'happy_to_have_you' => 'We are happy to have you with us. To complete your registration and activate your account, click the link below:',
+    'confirm_account' => 'Confirm Account',
+    'copy_link' => 'If the button above does not work, copy and paste the following link into your browser:',
+    'link_expires' => 'This link is personal and should not be shared. It expires in 24 hours.',
+    'ignore_email' => 'If you did not register, please ignore this email.',
+    'contact_us' => 'If you have any questions, we are at your disposal! Contact us via email',
+    'best_regards' => 'Best regards, SNAPHUB Team.',
+    'do_not_reply' => 'Do not reply to this email. It was generated automatically.',
+    'published_on' => 'Published on',
 
 ];
--- lang/es/email.php
+++ lang/es/email.php
@@ -2,15 +2,15 @@
 
 return [
 
-    'welcome' => 'Bem-vindo ao SNAPHUB!',
-    'happy_to_have_you' => 'Estamos felizes em ter você conosco. Para concluir seu cadastro e ativar sua conta, clique no link abaixo:',
-    'confirm_account' => 'Confirmar Conta',
-    'copy_link' => 'Caso o botão acima não funcione, copie e cole o seguinte link em seu navegador:',
-    'link_expires' => 'Este link é pessoal e não deve ser compartilhado. Ele expira em 24 horas.',
-    'ignore_email' => 'Se você não realizou este cadastro, ignore este e-mail.',
-    'contact_us' => 'Qualquer dúvida, estamos à disposição! Entre em contato pelo e-mail',
-    'best_regards' => 'Abraços, Equipe SNAPHUB.',
-    'do_not_reply' => 'Não responda a este e-mail. Ele foi gerado automaticamente.',
-    'published_on' => 'Publicado em',
+    'welcome' => '¡Bienvenido a SNAPHUB!',
+    'happy_to_have_you' => 'Estamos felices de tenerte con nosotros. Para completar tu registro y activar tu cuenta, haz clic en el enlace a continuación:',
+    'confirm_account' => 'Confirmar Cuenta',
+    'copy_link' => 'Si el botón anterior no funciona, copia y pega el siguiente enlace en tu navegador:',
+    'link_expires' => 'Este enlace es personal y no debe ser compartido. Expira en 24 horas.',
+    'ignore_email' => 'Si no realizaste este registro, ignora este correo electrónico.',
+    'contact_us' => 'Cualquier duda, estamos a tu disposición! Contáctanos por correo electrónico',
+    'best_regards' => 'Saludos, Equipo SNAPHUB.',
+    'do_not_reply' => 'No respondas a este correo electrónico. Fue generado automáticamente.',
+    'published_on' => 'Publicado en',
 
 ];
