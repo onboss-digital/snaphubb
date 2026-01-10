fd45a34d2d2f4fd2d685c6bd167de6b44ced55c2
onboss-digital <onbossdigital@gmail.com>
2026-01-10 18:04:52 -0300

    Fix PSR-4 autoloading issues and make Telescope provider safe for --no-dev installs

--- Modules/Subscriptions/database/migrations/2025_02_13_114416_add_cartpanda_fields_to_plans_table.php
+++ Modules/Subscriptions/database/migrations/2025_02_13_114416_add_cartpanda_fields_to_plans_table.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddCartpandaFieldsToPlansTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -22,4 +23,4 @@ class AddCartpandaFieldsToPlansTable extends Migration
             $table->dropColumn('cartpanda_checkout_url');
         });
     }
-}
+};
